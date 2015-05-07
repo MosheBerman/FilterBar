3695ec01a3f7c4667d6db5e027a1615b45281a2d
Moshe Berman <moshefromsinai@gmail.com>
2015-05-07 19:20:14 -0400

    Added a rounded label.

---
+++ Filter Bar Demo/RoundedLabel/RoundedLabel.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedLabel.swift
+//  Filter Bar Demo
+//
+//  Created by Moshe Berman on 5/7/15.
+//  Copyright (c) 2015 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RoundedLabel: UILabel {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.layer.cornerRadius = min(CGRectGetHeight(self.bounds), CGRectGetWidth(self.bounds)) / 2.0
+        self.clipsToBounds = true
+    }
+    
+}
