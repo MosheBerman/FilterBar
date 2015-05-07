6beea7fb055df9a058afed11707c72bb6bca483a
Moshe Berman <moshefromsinai@gmail.com>
2015-05-07 16:00:24 -0400

    Initial Commit

---
+++ Filter Bar Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filter Bar Demo
+//
+//  Created by Moshe Berman on 5/7/15.
+//  Copyright (c) 2015 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Filter Bar DemoTests/Filter_Bar_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Filter_Bar_DemoTests.swift
+//  Filter Bar DemoTests
+//
+//  Created by Moshe Berman on 5/7/15.
+//  Copyright (c) 2015 Moshe Berman. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Filter_Bar_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
