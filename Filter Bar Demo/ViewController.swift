b87c6748b9f195e488646b2e4e2c7135258be4ec
Moshe Berman <moshefromsinai@gmail.com>
2015-05-08 13:47:29 -0400

    Cleaned up the sample a little bit.

--- Filter Bar Demo/ViewController.swift
+++ Filter Bar Demo/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         filterBar.addTarget(self, action: "segmentChanged:", forControlEvents: .ValueChanged)
         
         //  This sets the titles of the filter bar.
-        filterBar.titles = ["Apples", "Bananas", "Cherries"]
+        filterBar.titles = ["Aragorn", "Bilbo", "Ceorl"]    // Lord of the Rings!
         
         //  This sets the color of the filter bar
         filterBar.color = UIColor.whiteColor()
@@ -31,13 +31,37 @@ class ViewController: UIViewController {
         //
         
         //  Style the navigation controller.
-        self.navigationController?.navigationBar.translucent = true
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.13, green: 0.20, blue: 0.62, alpha: 1.00)
         
         //  Set the bakground color of the view.
         self.view.backgroundColor = UIColor(red: 0.13, green: 0.20, blue: 0.62, alpha: 1.00)
         
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+
+        //
+        //  Create the filter
+        //
+        
+        let filter : FilterBar = FilterBar()
+        
+        //  Add button titles
+        filter.titles = ["Albus", "Bathilda", "Charlie", "Harry"]    // Harry Potter!
+        
+        //  Color the filter bar
+        filter.color = UIColor.whiteColor()
+        filter.borderColor = UIColor.whiteColor()
+        
+        //  Install the filter
+        self.view.addSubview(filter)
+        
+        //  Add a positioning constraint
+        let topConstraint : NSLayoutConstraint = NSLayoutConstraint(item: filter, attribute: .Top, relatedBy: .Equal, toItem: self.filterBar, attribute: .Bottom, multiplier: 1.0, constant: 8.0)
+        self.view.addConstraint(topConstraint)
+        filter.setTranslatesAutoresizingMaskIntoConstraints(false)
+        
+        // Handle event changes
+        filter.addTarget(self, action: "segmentChanged:", forControlEvents: .ValueChanged)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -50,7 +74,7 @@ class ViewController: UIViewController {
         
         let filter : FilterBar = sender as! FilterBar
         
-        let index : NSInteger = self.filterBar.selectedSegmentIndex
+        let index : NSInteger = filter.selectedSegmentIndex
         let string : String = filter.titles[index]
         
         self.displayLabel.text = String(format: "Segment %i : %@", index, string)
